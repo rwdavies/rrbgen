a1a04faa59c384b9bbb5803ecb0b87bd498ea6df
rwdavies <robertwilliamdavies@gmail.com>
2018-05-24 15:21:04 -0400

    Add tests for basic functionality

--- rrbgen/R/functions.R
+++ rrbgen/R/functions.R
@@ -54,114 +54,130 @@ load_bgen_header <- function(to.read) {
 }
 
 
-test <- function() {
 
-
-    file <- "~/Downloads/bgen/bgen/example/example.16bits.bgen"
-
-    ## convert to .bgen
-    ## try to read back in!
-    close(to.read)
-    to.read <- file(file, "rb")
-    
-
-
-## Variant data blocks
-if (Layout == 1) {
-    N <- readBin(to.read, integer(), endian = "little")
-}
-## variant ID
-L_id <- readBin(to.read, size = 2, "integer", n = 1, endian = "little")
-var_id <- readBin(to.read, size = 1, "raw", n = L_id, endian = "little")
-var_id_char <- rawToChar(var_id)
-## rsid
-L_rsid <- readBin(to.read, size = 2, "integer", n = 1, endian = "little")
-var_rsid <- readBin(to.read, size = 1, "raw", n = L_rsid, endian = "little")
-var_rsid_char <- rawToChar(var_rsid)
-## chromosome (char)
-L_chr <- readBin(to.read, size = 2, "integer", n = 1, endian = "little")
-var_chr <- readBin(to.read, size = 1, "raw", n = L_chr, endian = "little")
-var_chr_char <- rawToChar(var_chr)
-## variant position
-var_position <- readBin(to.read, size = 4, "integer", n = 1, endian = "little")
-## number of K-alleles
-num_K_alleles <- readBin(to.read, size = 2, "integer", n = 1, endian = "little")
-alleles <- array(NA, num_K_alleles)
-for(i_allele in 1:num_K_alleles) {
-    L_ai <- readBin(to.read, size = 4, "integer", n = 1, endian = "little")
-    ai_char <- rawToChar(readBin(to.read, size = 1, "raw", n = L_ai, endian = "little"))
-    alleles[i_allele] <- ai_char
-}
-
-## Genotype data block
-C <- readBin(to.read, size = 4, "integer", n = 1, endian = "little") ## length of rest of the data for this variant
-if (CompressedSNPBlocks > 0) {
-    D <- readBin(to.read, size = 4, "integer", n = 1, endian = "little") ## length after de-compression
-} else {
-    D <- C
-}
-data_compressed <- readBin(to.read, size = 1, "raw", n = C, endian = "little")
-
-if (CompressedSNPBlocks == 1) {
-    ## Indicates SNP block probability data is compressed using zlib's compress() function.
-    data <- memDecompress(data_compressed, type = "gzip")
-    dataC <- rawConnection(data)
-} else if (CompressedSNPBlocks == 0) {
-    dataC <- rawConnection(data_compressed)
-}
-new_N <- readBin(dataC, size = 4, "integer", n = 1, endian = "little")
-if (new_N != N) {
-    stop("bad interpretation")
-}
-new_num_K_alleles <- readBin(dataC, size = 2, "integer", n = 1, endian = "little")
-if (new_num_K_alleles != num_K_alleles) {
-    stop("bad interpretation")
-}
-p_min <- readBin(dataC, size = 1, "integer", n = 1, endian = "little")
-p_max <- readBin(dataC, size = 1, "integer", n = 1, endian = "little")
-N_bytes <- readBin(dataC, size = 1, "raw", n = N, endian = "little")
-## last bit is missingness
-is_missing <- as.logical(sapply(N_bytes, function(byte) rawToBits(byte)[8]))
-ploidy <- sapply(N_bytes, function(byte) sum(2 ** (0:6) * as.integer(rawToBits(byte)[1:7])))
-##
-phased_flag <- as.integer(readBin(dataC, size = 1, "raw", n = 1, endian = "little"))
-if ((phased_flag != 1) & (phased_flag != 0)) {
-    stop("problem with file, phased flag wrong")
-}
-bit_prob <- as.integer(readBin(dataC, size = 1, "raw", n = 1, endian = "little"))
-if ((bit_prob < 1) | (32 < bit_prob)) {
-    stop("bit prob is outside range")
+load_variant_identifying_data_for_one_snp <- function(to.read, Layout) {
+    ## Variant data blocks
+    if (Layout == 1) {
+        N <- readBin(to.read, integer(), endian = "little")
+    }
+    ## variant ID
+    L_id <- readBin(to.read, size = 2, "integer", n = 1, endian = "little")
+    var_id <- readBin(to.read, size = 1, "raw", n = L_id, endian = "little")
+    var_id_char <- rawToChar(var_id)
+    ## rsid
+    L_rsid <- readBin(to.read, size = 2, "integer", n = 1, endian = "little")
+    var_rsid <- readBin(to.read, size = 1, "raw", n = L_rsid, endian = "little")
+    var_rsid_char <- rawToChar(var_rsid)
+    ## chromosome (char)
+    L_chr <- readBin(to.read, size = 2, "integer", n = 1, endian = "little")
+    var_chr <- readBin(to.read, size = 1, "raw", n = L_chr, endian = "little")
+    var_chr_char <- rawToChar(var_chr)
+    ## variant position
+    var_position <- readBin(to.read, size = 4, "integer", n = 1, endian = "little")
+    ## re-produce 
+    ## number of K-alleles
+    num_K_alleles <- readBin(to.read, size = 2, "integer", n = 1, endian = "little")
+    alleles <- array(NA, num_K_alleles)
+    for(i_allele in 1:num_K_alleles) {
+        L_ai <- readBin(to.read, size = 4, "integer", n = 1, endian = "little")
+        ai_char <- rawToChar(readBin(to.read, size = 1, "raw", n = L_ai, endian = "little"))
+        alleles[i_allele] <- ai_char
+    }
+    if (num_K_alleles > 2) {
+        stop("num_K_alleles not supported for >2")
+    }
+    ## make a summary here, similar to .gen file
+    variant_info <- c(
+        chr = var_chr_char,
+        snpid = var_id_char,
+        rsid = var_rsid_char,
+        position = var_position,
+        ref = alleles[1],
+        alt = alleles[2]
+    )
+    return(
+        list(
+            variant_info = variant_info,
+            num_K_alleles = num_K_alleles
+        )
+    )
 }
 
-if (phased_flag == 1) {
-    ## haplotype probabilities
-    stop("write this")
-} else if (phased == 0) {
 
-    remaining_bytes <- D - 4 - 2 - 1 - 1 - N - 1 - 1
-    dosage <- array(NA, N)
-    gen_probs <- array(NA, c(3, N))
-    if ((bit_prob %in% c(8, 16, 24, 32)) == FALSE) {
+load_genotypes_for_one_snp <- function(to.read, num_K_alleles, N, CompressedSNPBlocks) {
+    ## Genotype data block
+    C <- readBin(to.read, size = 4, "integer", n = 1, endian = "little") ## length of rest of the data for this variant
+    if (CompressedSNPBlocks > 0) {
+        D <- readBin(to.read, size = 4, "integer", n = 1, endian = "little") ## length after de-compression
+    } else {
+        D <- C
+    }
+    data_compressed <- readBin(to.read, size = 1, "raw", n = C, endian = "little")
+    if (CompressedSNPBlocks == 1) {
+        ## Indicates SNP block probability data is compressed using zlib's compress() function.
+        data <- memDecompress(data_compressed, type = "gzip")
+        dataC <- rawConnection(data)
+    } else if (CompressedSNPBlocks == 0) {
+        dataC <- rawConnection(data_compressed)
+    }
+    new_N <- readBin(dataC, size = 4, "integer", n = 1, endian = "little")
+    if (new_N != N) {
+        stop("bad interpretation")
+    }
+    new_num_K_alleles <- readBin(dataC, size = 2, "integer", n = 1, endian = "little")
+    if (new_num_K_alleles != num_K_alleles) {
+        stop("bad interpretation")
+    }
+    p_min <- readBin(dataC, size = 1, "integer", n = 1, endian = "little")
+    p_max <- readBin(dataC, size = 1, "integer", n = 1, endian = "little")
+    N_bytes <- readBin(dataC, size = 1, "raw", n = N, endian = "little")
+    ## last bit is missingness
+    is_missing <- as.logical(sapply(N_bytes, function(byte) rawToBits(byte)[8]))
+    ploidy <- sapply(N_bytes, function(byte) sum(2 ** (0:6) * as.integer(rawToBits(byte)[1:7])))
+    ##
+    phased_flag <- as.integer(readBin(dataC, size = 1, "raw", n = 1, endian = "little"))
+    if ((phased_flag != 1) & (phased_flag != 0)) {
+        stop("problem with file, phased flag wrong")
+    }
+    bit_prob <- as.integer(readBin(dataC, size = 1, "raw", n = 1, endian = "little"))
+    if ((bit_prob < 1) | (32 < bit_prob)) {
+        stop("bit prob is outside range")
+    }
+    if (phased_flag == 1) {
+        ## haplotype probabilities
+        stop("haplotype parsing not written")
+    } else if (phased_flag == 0) {
+        ##
+        remaining_bytes <- D - 4 - 2 - 1 - 1 - N - 1 - 1
+        dosage <- array(NA, N)
+        gen_probs <- array(NA, c(3, N))
+        if ((bit_prob %in% c(8, 16, 24, 32)) == FALSE) {
         stop("non multiple of 8 bit_prob not supported")
-    }
-    for(iSample in 1:N) {
-        if (ploidy[iSample] != 2) {
-            stop("this code does not support non-2 ploidy")
         }
-        b_hom_ref <- readBin(dataC, size = 1, "raw", n = bit_prob / 8, endian = "little")
-        b_het <- readBin(dataC, size = 1, "raw", n = bit_prob / 8, endian = "little")        
-        if (is_missing[iSample]) {
-            dosage[iSample] <- NA
-            gen_probs[1:3, iSample] <- NA
-        } else {
-            x <- sum(as.integer(rawToBits(b_hom_ref)) * (2 ** (0:(bit_prob - 1)))    )
-            p_hom_ref <- x / (2 ** bit_prob)
-            x <- sum(as.integer(rawToBits(b_het)) * (2 ** (0:(bit_prob - 1)))    )
-            p_het <- x / (2 ** bit_prob)
-            p_hom_alt <- 1 - p_hom_ref - p_het
-            dosage[iSample] <- p_het * 2 * p_hom_alt
-            gen_probs[1:3, iSample] <- c(p_hom_ref, p_het, p_hom_alt)
+        for(iSample in 1:N) {
+            if (ploidy[iSample] != 2) {
+                stop("this code does not support non-2 ploidy")
+            }
+            b_hom_ref <- readBin(dataC, size = 1, "raw", n = bit_prob / 8, endian = "little")
+            b_het <- readBin(dataC, size = 1, "raw", n = bit_prob / 8, endian = "little")        
+            if (is_missing[iSample]) {
+                dosage[iSample] <- NA
+                gen_probs[1:3, iSample] <- NA
+            } else {
+                x <- sum(as.integer(rawToBits(b_hom_ref)) * (2 ** (0:(bit_prob - 1)))    )
+                p_hom_ref <- x / (2 ** bit_prob)
+                x <- sum(as.integer(rawToBits(b_het)) * (2 ** (0:(bit_prob - 1)))    )
+                p_het <- x / (2 ** bit_prob)
+                p_hom_alt <- 1 - p_hom_ref - p_het
+                dosage[iSample] <- p_het * 2 * p_hom_alt
+                gen_probs[1:3, iSample] <- c(p_hom_ref, p_het, p_hom_alt)
+            }
         }
     }
-}
+    return(
+        list(
+            dosage = dosage,
+            gen_probs = gen_probs
+        )
+    )
 }
--- rrbgen/tests/testthat/test-external.R
+++ rrbgen/tests/testthat/test-external.R
@@ -1,19 +1,58 @@
 ## tests using external data from bgen library
 ## note - these are run in rrbgen/rrbgen/tests/testthat
 ## need to go back 3 directories
+## library('testthat'); setwd("~/Dropbox/rrbgen/rrbgen/tests/testthat")
 external_bgen_dir <- "../../../external/bgen/"
 bgen_file <- file.path(external_bgen_dir, "example.16bits.bgen")
 gen_file <- file.path(external_bgen_dir, "example.gen")    
 gen <- read.table(gen_file)
 
+## source("~/Dropbox/rrbgen/rrbgen/R/functions.R")
+
 test_that("can test something", {
 
     ## close(to.read)
     to.read <- file(bgen_file, "rb")
-    bgen_header <- load_bgen_header(to.read)
-    close(to.read)
 
-    ## expect_equal(1, 2)    
+    ## header
+    bgen_header <- load_bgen_header(to.read)
+    Layout <- bgen_header$Layout
+    CompressedSNPBlocks <- bgen_header$CompressedSNPBlocks
+    N <- bgen_header$N
     expect_equal(bgen_header$N, 500)
 
+    ## variant
+    out <- load_variant_identifying_data_for_one_snp(to.read, Layout)
+    variant_info <- out$variant_info
+    num_K_alleles <- out$num_K_alleles
+    expect_equal(as.integer(variant_info["chr"]), gen[1, 1])
+    expect_equal(as.character(variant_info["snpid"]), as.character(gen[1, 2]))
+    expect_equal(as.character(variant_info["rsid"]), as.character(gen[1, 3]))
+    expect_equal(as.character(variant_info["position"]), as.character(gen[1, 4]))
+    expect_equal(as.character(variant_info["ref"]), as.character(gen[1, 5]))
+    expect_equal(as.character(variant_info["alt"]), as.character(gen[1, 6]))
+
+    ## genotypes
+    out <- load_genotypes_for_one_snp(to.read, num_K_alleles, N, CompressedSNPBlocks) 
+
+    tolerance <- 1e-4
+    ## check a few 
+    expect_equal(
+        is.na(out$gen_probs[, 1]),
+        as.logical(gen[1, 7:9] == 0)
+    )
+    ## 
+    expect_equal(
+        as.logical(abs(out$gen_probs[, 2] - gen[1, 10:12]) > tolerance),
+        rep(FALSE, 3)
+    )
+    ## 
+    expect_equal(
+        as.logical(abs(out$gen_probs[, 3] - gen[1, 13:15]) > tolerance),
+        rep(FALSE, 3)
+    )
+
+    close(to.read)
+
 })
+
