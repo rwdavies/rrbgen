25372f5ee9725e185c1b6fe76428778c07f9bb81
Robert Davies <robert davies@rt4789ml-cblg.sickkids.ca>
2018-05-24 12:29:50 -0400

    Add R dependencies

---
+++ scripts/install-r-dependencies.R
@@ -0,0 +1,10 @@
+#!/usr/bin/env Rscript
+
+if (!suppressPackageStartupMessages(require("Rcpp")))
+    install.packages("Rcpp", repos="http://cran.rstudio.com/")
+if (!suppressPackageStartupMessages(require("devtools")))
+    install.packages("devtools", repos="http://cran.rstudio.com/")
+if (!suppressPackageStartupMessages(require("testthat")))
+    install.packages("testthat", repos="http://cran.rstudio.com/")
+if (!suppressPackageStartupMessages(require("roxygen2")))
+    install.packages("roxygen2", repos="http://cran.rstudio.com/")
