a1560f50470b47544cebc8425175f7f303599ad4
rwdavies <robertwilliamdavies@gmail.com>
2018-05-24 17:36:13 -0400

    Can do 3 things now - load samples, SNPs and all genotypes

--- rrbgen/R/functions.R
+++ rrbgen/R/functions.R
@@ -1,11 +1,45 @@
 ## here is the general purpose API
+## later, instruct only loading certain samples or snps
 ## have separate samples and SNPs below
 rrbgen_load <- function(
-    file,
-    samples = NULL,
-    snps = NULL
+    bgen_file
 ) {
-    print("TODO")
+    ## here load all samples and variants, unless instructed otherwise
+    to.read <- file(bgen_file, "rb")
+    ## header
+    bgen_header <- load_bgen_header(to.read)
+    offset <- bgen_header$offset
+    L_H <- bgen_header$L_H
+    Layout <- bgen_header$Layout    
+    SampleIdentifiers <- bgen_header$SampleIdentifiers
+    M <- bgen_header$M
+    N <- bgen_header$N
+    CompressedSNPBlocks <- bgen_header$CompressedSNPBlocks    
+    ## samples
+    out <- load_bgen_sample_identifier_block(to.read, L_H + 4, SampleIdentifiers, N)
+    sample_names <- out$sample_names
+    ## snp info
+    out <- load_variant_identifying_data_for_all_snps(to.read, offset, Layout, M, CompressedSNPBlocks)
+    snp_info <- out$snp_info
+    per_var_offset <- out$per_var_offset
+    per_var_C <- out$per_var_C
+    per_var_L_vid <- out$per_var_L_vid
+    per_var_num_K_alleles <- out$per_var_num_K_alleles
+    ## load genotypes
+    gp <- array(NA, c(M, N, 3))
+    for(i_var in 1:M) {
+        binary_start <- per_var_offset[i_var] + per_var_L_vid[i_var]
+        C <- per_var_C[i_var]
+        num_K_alleles <- per_var_num_K_alleles[i_var]
+        out <- load_genotypes_for_one_snp(to.read, binary_start, num_K_alleles, N, CompressedSNPBlocks, C)
+        gp[i_var, , ] <- out$gen_probs
+    }
+    return(
+        list(
+            gp = gp,
+            sample_names = sample_names
+        )
+    )
 }
 
 
@@ -28,31 +62,20 @@ rrbgen_load_samples <- function(
 }
 
 
-## just load the SNP information
-rrbgen_load_snps <- function(
-    file
+## just load the variant information
+rrbgen_load_variant_info <- function(
+    bgen_file
 ) {
-
-    close(to.read)
     to.read <- file(bgen_file, "rb")
+    ## header
     bgen_header <- load_bgen_header(to.read)
-    
-    L_H <- bgen_header$L_H ## length of header in bytes
+    offset <- bgen_header$offset
     Layout <- bgen_header$Layout
     CompressedSNPBlocks <- bgen_header$CompressedSNPBlocks
     M <- bgen_header$M
-    N <- bgen_header$N
-
-    out <- load_bgen_sample_identifier_block(to.read, SampleIdentifiers, N)     
-    seek(to.read, where =NA)    
-    
-    ##
-    for(i_m in 1:M) {
-        out <- load_variant_identifying_data_for_one_snp(to.read, Layout)
-        variant_info <- out$variant_info
-        num_K_alleles <- out$num_K_alleles
-    }
-
+    ## load all variants
+    out <- load_variant_identifying_data_for_all_snps(to.read, offset, Layout, M, CompressedSNPBlocks)
+    return(out$snp_info)
 }
 
 
@@ -118,10 +141,55 @@ load_bgen_sample_identifier_block <- function(to.read, binary_start, SampleIdent
     )
 }
 
-load_variant_identifying_data_for_one_snp <- function(to.read, Layout) {
+
+load_variant_identifying_data_for_all_snps <- function(to.read, offset, Layout, M, CompressedSNPBlocks) {
+    ## first one starts at offset + 4
+    ## subsequent ones depend!
+    per_var_offset <- array(NA, M)
+    per_var_offset[1] <- offset + 4
+    per_var_C <- array(NA, M)
+    per_var_L_vid <- array(NA, M)
+    per_var_num_K_alleles <- array(NA, M)
+    ## 
+    snp_info <- array(NA, c(M, 6))
+    colnames(snp_info) <- c("chr", "snpid", "rsid", "position", "ref", "alt")
+    ##     
+    for(i_var in 1:M) {
+        out <- load_variant_identifying_data_for_one_snp(to.read, per_var_offset[i_var], Layout, CompressedSNPBlocks)
+        per_var_C[i_var] <- out$C
+        per_var_L_vid[i_var] <- out$L_vid
+        per_var_num_K_alleles[i_var] <- out$num_K_alleles
+        if (i_var < M) {
+            per_var_offset[i_var + 1] <- per_var_offset[i_var] + out$L_vid
+            if (CompressedSNPBlocks == 1) {
+                per_var_offset[i_var + 1] <- per_var_offset[i_var + 1] + out$C - 4
+            } else if (CompressedSNPBlocks == 0) {
+                per_var_offset[i_var + 1] <- per_var_offset[i_var + 1] + out$C
+            }
+        }
+        snp_info[i_var, ] <- out$variant_info
+    }
+    return(
+        list(
+            snp_info = snp_info,
+            per_var_offset = per_var_offset,
+            per_var_C = per_var_C,
+            per_var_L_vid = per_var_L_vid,
+            per_var_num_K_alleles = per_var_num_K_alleles
+        )
+    )
+}
+
+
+
+## Note I am including C and/or D from the genotype data block in this
+load_variant_identifying_data_for_one_snp <- function(to.read, binary_start, Layout, CompressedSNPBlocks) {
+    seek(to.read, where = binary_start)
+    L_vid <- 0
     ## Variant data blocks
     if (Layout == 1) {
         N <- readBin(to.read, integer(), endian = "little")
+        L_vid <- L_vid + 4
     }
     ## variant ID
     L_id <- readBin(to.read, size = 2, "integer", n = 1, endian = "little")
@@ -141,8 +209,9 @@ load_variant_identifying_data_for_one_snp <- function(to.read, Layout) {
     ## number of K-alleles
     num_K_alleles <- readBin(to.read, size = 2, "integer", n = 1, endian = "little")
     alleles <- array(NA, num_K_alleles)
+    L_ai <- array(NA, num_K_alleles)
     for(i_allele in 1:num_K_alleles) {
-        L_ai <- readBin(to.read, size = 4, "integer", n = 1, endian = "little")
+        L_ai[i_allele] <- readBin(to.read, size = 4, "integer", n = 1, endian = "little")
         ai_char <- rawToChar(readBin(to.read, size = 1, "raw", n = L_ai, endian = "little"))
         alleles[i_allele] <- ai_char
     }
@@ -158,23 +227,38 @@ load_variant_identifying_data_for_one_snp <- function(to.read, Layout) {
         ref = alleles[1],
         alt = alleles[2]
     )
+    ## now, also, read next 4 bytes, to get C and D, from the Genotype Data block
+    C <- readBin(to.read, size = 4, "integer", n = 1, endian = "little") ## length of rest of the data for this variant
+    if (Layout == 2) {    
+        if (CompressedSNPBlocks > 0) {
+            D <- readBin(to.read, size = 4, "integer", n = 1, endian = "little") ## length after de-compression
+            L_vid <- L_vid + 4
+        } else {
+            D <- C
+        }
+    }
+    ## get the total number of bytes this used. note this now includes C and D
+    L_vid <- L_vid + 16 + 4 * num_K_alleles + L_id + L_rsid + L_chr + sum(L_ai)
+    if (Layout == 1) {
+        L_vid <- L_vid + 4
+    }
     return(
         list(
             variant_info = variant_info,
-            num_K_alleles = num_K_alleles
+            num_K_alleles = num_K_alleles,
+            C = C,
+            D = D,
+            L_vid = L_vid
         )
     )
 }
 
 
-load_genotypes_for_one_snp <- function(to.read, num_K_alleles, N, CompressedSNPBlocks) {
+## does not include C or D
+## this comes from 
+load_genotypes_for_one_snp <- function(to.read, binary_start, num_K_alleles, N, CompressedSNPBlocks, C) {
+    seek(to.read, where = binary_start)    
     ## Genotype data block
-    C <- readBin(to.read, size = 4, "integer", n = 1, endian = "little") ## length of rest of the data for this variant
-    if (CompressedSNPBlocks > 0) {
-        D <- readBin(to.read, size = 4, "integer", n = 1, endian = "little") ## length after de-compression
-    } else {
-        D <- C
-    }
     data_compressed <- readBin(to.read, size = 1, "raw", n = C, endian = "little")
     if (CompressedSNPBlocks == 1) {
         ## Indicates SNP block probability data is compressed using zlib's compress() function.
@@ -211,9 +295,9 @@ load_genotypes_for_one_snp <- function(to.read, num_K_alleles, N, CompressedSNPB
         stop("haplotype parsing not written")
     } else if (phased_flag == 0) {
         ##
-        remaining_bytes <- D - 4 - 2 - 1 - 1 - N - 1 - 1
-        dosage <- array(NA, N)
-        gen_probs <- array(NA, c(3, N))
+        ## remaining_bytes <- D - 4 - 2 - 1 - 1 - N - 1 - 1
+        ## dosage <- array(NA, N)
+        gen_probs <- array(NA, c(N, 3))
         if ((bit_prob %in% c(8, 16, 24, 32)) == FALSE) {
         stop("non multiple of 8 bit_prob not supported")
         }
@@ -224,22 +308,23 @@ load_genotypes_for_one_snp <- function(to.read, num_K_alleles, N, CompressedSNPB
             b_hom_ref <- readBin(dataC, size = 1, "raw", n = bit_prob / 8, endian = "little")
             b_het <- readBin(dataC, size = 1, "raw", n = bit_prob / 8, endian = "little")        
             if (is_missing[iSample]) {
-                dosage[iSample] <- NA
-                gen_probs[1:3, iSample] <- NA
+                ## dosage[iSample] <- NA
+                gen_probs[iSample, 1:3] <- NA
             } else {
                 x <- sum(as.integer(rawToBits(b_hom_ref)) * (2 ** (0:(bit_prob - 1)))    )
                 p_hom_ref <- x / (2 ** bit_prob)
                 x <- sum(as.integer(rawToBits(b_het)) * (2 ** (0:(bit_prob - 1)))    )
                 p_het <- x / (2 ** bit_prob)
                 p_hom_alt <- 1 - p_hom_ref - p_het
-                dosage[iSample] <- p_het * 2 * p_hom_alt
-                gen_probs[1:3, iSample] <- c(p_hom_ref, p_het, p_hom_alt)
+                ## dosage[iSample] <- p_het * 2 * p_hom_alt
+                gen_probs[iSample, 1:3] <- c(p_hom_ref, p_het, p_hom_alt)
             }
         }
     }
+    close(dataC)
+    rm(data)
     return(
         list(
-            dosage = dosage,
             gen_probs = gen_probs
         )
     )
--- rrbgen/tests/testthat/test-external.R
+++ rrbgen/tests/testthat/test-external.R
@@ -14,6 +14,7 @@ sample_ids_from_sample_file <- as.character(sample[-c(1:2), ])
 
 test_that("can test things", {
 
+    ##
     ## source("~/Dropbox/rrbgen/rrbgen/R/functions.R");     close(to.read)
     to.read <- file(bgen_file, "rb")
 
@@ -25,20 +26,26 @@ test_that("can test things", {
     M <- bgen_header$M
     N <- bgen_header$N
     SampleIdentifiers <- bgen_header$SampleIdentifiers
+    offset <- bgen_header$offset
     expect_equal(bgen_header$N, 500)
 
     ## sample identifier block
     out <- load_bgen_sample_identifier_block(to.read, L_H + 4, SampleIdentifiers, N)
     sample_names <- out$sample_names
+    L_SI <- out$L_SI
     expect_equal(
         (sample_ids_from_sample_file),
         as.character(sample_names)
     )
 
+    
     ## variant
-    out <- load_variant_identifying_data_for_one_snp(to.read, Layout)
+    out <- load_variant_identifying_data_for_one_snp(to.read, L_SI + L_H + 4, Layout, CompressedSNPBlocks)
     variant_info <- out$variant_info
     num_K_alleles <- out$num_K_alleles
+    L_vid <- out$L_vid
+    C <- out$C
+    D <- out$D
     expect_equal(as.integer(variant_info["chr"]), gen[1, 1])
     expect_equal(as.character(variant_info["snpid"]), as.character(gen[1, 2]))
     expect_equal(as.character(variant_info["rsid"]), as.character(gen[1, 3]))
@@ -47,22 +54,22 @@ test_that("can test things", {
     expect_equal(as.character(variant_info["alt"]), as.character(gen[1, 6]))
 
     ## genotypes
-    out <- load_genotypes_for_one_snp(to.read, num_K_alleles, N, CompressedSNPBlocks) 
+    out <- load_genotypes_for_one_snp(to.read, offset + 4 + L_vid, num_K_alleles, N, CompressedSNPBlocks, C) 
 
     tolerance <- 1e-4
     ## check a few 
     expect_equal(
-        is.na(out$gen_probs[, 1]),
+        is.na(out$gen_probs[1, ]),
         as.logical(gen[1, 7:9] == 0)
     )
     ## 
     expect_equal(
-        as.logical(abs(out$gen_probs[, 2] - gen[1, 10:12]) > tolerance),
+        as.logical(abs(out$gen_probs[2, ] - gen[1, 10:12]) > tolerance),
         rep(FALSE, 3)
     )
     ## 
     expect_equal(
-        as.logical(abs(out$gen_probs[, 3] - gen[1, 13:15]) > tolerance),
+        as.logical(abs(out$gen_probs[3, ] - gen[1, 13:15]) > tolerance),
         rep(FALSE, 3)
     )
 
@@ -78,5 +85,36 @@ test_that("can load sample names", {
         (sample_ids_from_sample_file),
         as.character(sample_names)
     )
+
+})
+
+test_that("can load SNPs", {
+    
+    var_info <- rrbgen_load_variant_info(bgen_file)
+
+    expect_equal(as.integer(var_info[, 1]), gen[, 1])
+    for(i in 2:6) {
+        expect_equal(var_info[, i], as.character(gen[, i]))
+    }
+
+})
+
+test_that("can load everything", {
+
+    out <- rrbgen_load(bgen_file)
+    gp <- out$gp
+
+    ## check all genotypes below
+    tolerance <- 1e-4
+    gen1 <- gen[, 6 + seq(1, ncol(gen) - 6, 3)]
+    gen2 <- gen[, 6 + seq(2, ncol(gen) - 6, 3)]
+    gen3 <- gen[, 6 + seq(3, ncol(gen) - 6, 3)]
+
+    expect_equal(max(abs(gen1 - gp[, , 1]), na.rm = TRUE) < tolerance, TRUE)
+    expect_equal(max(abs(gen2 - gp[, , 2]), na.rm = TRUE) < tolerance, TRUE)
+    expect_equal(max(abs(gen3 - gp[, , 3]), na.rm = TRUE) < tolerance, TRUE)
+
+    ## check missing
+    expect_equal(is.na(gp[, , 1]), (gen1 == 0) & (gen2 == 0) & (gen3 == 0), check.attributes = FALSE)
 
 })
